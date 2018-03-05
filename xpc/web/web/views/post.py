215a69c8a1c97c3b77d998fa1b581fd93f6eb0a7
colorfulcomemonochrome <13664069165@163.com>
2018-03-05 11:54:47 +0800

    upgrade

--- xpc/web/web/views/post.py
+++ xpc/web/web/views/post.py
@@ -1,12 +1,26 @@
+import redis
+from django.utils.functional import cached_property
 from web.models.post import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from web.models.comment import Comment
 from web.helpers.composer import get_posts_by_cid
 
+r = redis.Redis()
+
+@cached_property
+def count(self):
+    posts_count = r.get('posts_count')
+    if not posts_count:
+        posts_count = self.object_list.count()
+        r.set('posts_count', posts_count)
+    return int(posts_count)
+
+Paginator.count = count
+
 
 def show_list(request):
-    post_list = Post.objects.order_by('-like_counts')
+    post_list = Post.objects.order_by('-play_counts')
     paginator = Paginator(post_list, 40)
     posts = paginator.page(1)
     return render(request, 'post_list.html', {'posts': posts})
